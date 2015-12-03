fde879146e581126406f359f70715948af303878
Le-Yo <lenykoskey@yahoo.com>
2015-12-03 17:46:53 +0300

    initial commit

---
+++ src/rapidussdServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace leyo\rapidussd;
+
+use Illuminate\Support\ServiceProvider;
+
+class rapidussdServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
